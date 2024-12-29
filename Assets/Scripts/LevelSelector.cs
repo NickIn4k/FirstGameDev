dabe0650d6b0e3d9b9ebf20a7db9da3da33ee67a
Creazzo Nicola <nicola.creazzo.007@gmail.com>
2024-12-29 17:57:42 +0100

    Level selector added

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int SceneIndex;
+    public Canvas canvas;
+    public TextMeshProUGUI hoverText; 
+    public string message;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hoverText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Hai cliccato su: {gameObject.name}. Caricamento scena numero: {SceneIndex}");
+        //SceneManager.LoadScene(SceneIndex);
+    }
+
+    private void OnMouseEnter()
+    {
+        hoverText.text = message;
+        hoverText.gameObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        hoverText.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/RotateOnCommand.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class RotateOnCommand : MonoBehaviour
+{
+    public float Speed;
+    private Quaternion Rotazione; //Quaternion => oggetto per le rotazioni di qualsiasi tipo
+    private bool InRotazione;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InRotazione = false;
+        Rotazione = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InRotazione)
+        {
+            AspettaRotazione();
+            return;
+        }      
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            Ruota(90); 
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            Ruota(-90);
+    }
+    private void Ruota(int angolo)
+    {
+        Rotazione = Quaternion.Euler(0, transform.eulerAngles.y + angolo, 0);
+        InRotazione = true;
+    }
+
+    private void AspettaRotazione()
+    {        
+        transform.rotation = Quaternion.Slerp(transform.rotation, Rotazione, Time.deltaTime * Speed);   // Interpola verso la rotazione target
+
+        if (Quaternion.Angle(transform.rotation, Rotazione) < 0.5f)
+        {
+            transform.rotation = Rotazione; // Imposta esattamente la rotazione target
+            InRotazione = false;
+        }
+    }
+}
