c863a99aaece592bb8196716f74d77603d0f24bc
Cristian Jalba <cristianjalba4@gmail.com>
2025-01-28 17:23:03 +0100

    v0.2p5
    - Aggiunto un modello spingibile dal giocatore (sarà un NPC)(NON TOCCARE!)

---
+++ Assets/Scripts/AIScripts/Friendly/GroundPhysics.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GroundPhysics : MonoBehaviour
+{
+    bool isGrounded;
+    public float groundDrag;
+    public LayerMask ground;
+
+    Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkGrounded();
+
+        if (isGrounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+    }
+
+    void checkGrounded()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, GetComponent<CapsuleCollider>().bounds.size.y * 0.5f + 0.2f, ground))
+        {
+            isGrounded = true;
+        }
+        else
+            isGrounded = false;
+    }
+}
