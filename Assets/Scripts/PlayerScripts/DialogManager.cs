c90d1605c4a7c9356ea734910166902b995fe7f6
Creazzo Nicola <nicola.creazzo.007@gmail.com>
2025-01-30 19:56:18 +0100

    v0.2p13 - Dialogue Basis
    Added:
    - new panel
    - new animator controller
    - new animations
    Changed:
    - Movemnt.cs
    - DialogManager.cs

--- Assets/Scripts/PlayerScripts/DialogManager.cs
+++ Assets/Scripts/PlayerScripts/DialogManager.cs
@@ -1,21 +1,66 @@
 using Settings;
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DialogManager : MonoBehaviour
 {
-    void Start()
+    public Transform player;  // Riferimento al player
+    public Transform target;  // Riferimento all'oggetto da controllare
+    public GameObject DialogUI;
+    public float detectionRadius; // Distanza minima per considerare il player vicino
+    public GameObject Player;
+
+    Animator animator;
+    Animator playerAnimator;
+
+    private Movement playerMovementScript;
+    private string[] frasi = {"A", "AA", "AAA" };
+
+    private void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        playerMovementScript = player.GetComponent<Movement>();
+        playerAnimator = player.GetComponentInChildren<Animator>();
     }
 
-    void OnTriggerEnter(Collider other)
+    async void Update()
     {
-        if (other.CompareTag("Player")) //Controlla se è il player a toccare il trigger
+        if (player == null || target == null || playerMovementScript == null)
         {
-            //AGGIUNGERE ANIMAZIONE E DIALOGO
-            Debug.Log("DIALOGO");
+            Debug.LogWarning("Player, target o movement non assegnati!");
+            return;
+        }
+
+        float distance = Vector3.Distance(player.position, target.position);
+
+        if (distance <= detectionRadius)
+        {
+            detectionRadius = -1;
+            Debug.Log("Il player è vicino all'oggetto!");
+
+            animator.SetBool("isTalking", true);
+            playerAnimator.SetBool("isInDialog", true);
+            DialogUI.SetActive(true);
+
+            playerMovementScript.enabled = false;
+
+            await ShowTexts();
+
+            playerMovementScript.enabled = true;
+            animator.SetBool("isTalking", false);
+            playerAnimator.SetBool("isInDialog", false);
+            DialogUI.SetActive(false);
+        }
+    }
+    private async Task ShowTexts()
+    {
+        for(int i=0; i<frasi.Length; i++)
+        {
+            await Task.Delay(1000);
+            Debug.Log(frasi[i]);
+            //Cambia testo dentro al panel
         }
     }
 }
