3e9efe2dab9ecd67ac1a49a5737f6401b3a79aa2
Creazzo Nicola <nicola.creazzo.007@gmail.com>
2025-01-30 21:23:28 +0100

    v0.2p14 - Dialogues
    Added:
    - script functionalities
    - clickable screen to skip dialogues
    - icons
    Changed:
    - MoveToNextLevel.cs
    - DialogManager.cs

--- Assets/Scripts/MenuScripts/MoveToNextLevel.cs
+++ Assets/Scripts/MenuScripts/MoveToNextLevel.cs
@@ -25,8 +25,8 @@ public class MoveToNextLevel : MonoBehaviour
                 Debug.Log("HAI VINTO!!");
             else    //Se ci sono ancora livelli da sbloccare
             {
-                //if (Enum.IsDefined(typeof(CursorUnlockedScenes), nextSceneLoad)) //Sblocca il cursore se non è un livello
-                //    CursorSettings.Unlock();
+                if (Enum.IsDefined(typeof(CursorUnlockedScenes), nextSceneLoad)) //Sblocca il cursore se non è un livello
+                    CursorSettings.Unlock();
                 //SceneManager.LoadScene(nextSceneLoad);  //Carico il livello successivo
 
                 if(nextSceneLoad > PlayerPrefs.GetInt("levelAt"))   //Legge l'ultimo livello sbloccato
@@ -40,50 +40,3 @@ public class MoveToNextLevel : MonoBehaviour
         }
     }
 }
-
-
-
-/*
-using Settings;
-using System;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public enum CursorUnlockedScenes 
-{
-    MapSelector = 1,
-}
-
-public class MoveToNextLevel : MonoBehaviour
-{
-    public int nextSceneLoad;
-
-    void Start()
-    {
-        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) //Controlla se è il player a toccare il trigger
-        {
-            if(SceneManager.GetActiveScene().buildIndex == 5)   //Se completo l'ultimo livello (5 è l'int del livello 3)
-                Debug.Log("HAI VINTO!!");
-            else    //Se ci sono ancora livelli da sbloccare
-            {
-                if (Enum.IsDefined(typeof(CursorUnlockedScenes), nextSceneLoad)) //Sblocca il cursore se non è un livello
-                    CursorSettings.Unlock();
-                SceneManager.LoadScene(nextSceneLoad);  //Carico il livello successivo
-
-                if(nextSceneLoad > PlayerPrefs.GetInt("levelAt"))   //Legge l'ultimo livello sbloccato
-                {   //Se ho sbloccato un nuovo livello lo salvo
-                    PlayerPrefs.SetInt("levelAt", nextSceneLoad);
-                }
-            }
-
-            CursorSettings.Unlock();
-            SceneManager.LoadScene(1);
-        }
-    }
-}
-*/
--- Assets/Scripts/PlayerScripts/DialogManager.cs
+++ Assets/Scripts/PlayerScripts/DialogManager.cs
@@ -1,31 +1,57 @@
-using Settings;
-using System;
-using System.Threading.Tasks;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
+using TMPro;
+using GLTF.Schema;
+using Mono.Cecil.Cil;
+using static Unity.Burst.Intrinsics.X86;
+using static UnityEngine.InputManagerEntry;
+using System.ComponentModel;
+using System.Net.Sockets;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor;
+using UnityEngine.Device;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 public class DialogManager : MonoBehaviour
 {
-    public Transform player;  // Riferimento al player
-    public Transform target;  // Riferimento all'oggetto da controllare
+    public Transform player;
+    public Transform target;
     public GameObject DialogUI;
-    public float detectionRadius; // Distanza minima per considerare il player vicino
+    public float detectionRadius;
     public GameObject Player;
-
     Animator animator;
     Animator playerAnimator;
-
     private Movement playerMovementScript;
-    private string[] frasi = {"A", "AA", "AAA" };
+
+    public TextMeshProUGUI textComponent;
+    public float TextSpeed;
+    int index;
+    private readonly string[] lines = { 
+        "Lyra: 'Oh, a human! You don’t see many of us around here...'",
+        "Lyra: 'Sorry, my name is Lyra, nice to meet you! I’m part of a rebel group fighting against W.R.A.T.H.!'" ,
+        "Lira: 'What are you doing here?'",
+        "Lyra: 'You haven’t seen my team, have you? I hope they didn’t leave me behind...'",
+        "Lyra: 'Please, help me find them! They must be inside that tower down there!'",
+        "Lyra: 'To open the door, you need to find the secret code, but don’t worry...'",
+        "Lyra: 'Somewhere around here, I left my hacking device'",
+        "Lyra: 'It can be used to access the screens and reveal the secret codes...'",
+        "Lyra: 'Try looking around behind this house, or rather, what’s left of it...'"
+    };
+
+    private bool isDialogueActive = false;
 
     private void Start()
     {
         animator = GetComponentInChildren<Animator>();
         playerMovementScript = player.GetComponent<Movement>();
         playerAnimator = player.GetComponentInChildren<Animator>();
+        textComponent.text = string.Empty;
     }
 
-    async void Update()
+    void Update()
     {
         if (player == null || target == null || playerMovementScript == null)
         {
@@ -35,32 +61,65 @@ public class DialogManager : MonoBehaviour
 
         float distance = Vector3.Distance(player.position, target.position);
 
-        if (distance <= detectionRadius)
+        if (distance <= detectionRadius && !isDialogueActive)
+            StartCoroutine(StartDialogue());
+
+        if (isDialogueActive && Input.GetMouseButtonDown(0)) // Se clicca, avanza il dialogo
         {
-            detectionRadius = -1;
-            Debug.Log("Il player è vicino all'oggetto!");
-
-            animator.SetBool("isTalking", true);
-            playerAnimator.SetBool("isInDialog", true);
-            DialogUI.SetActive(true);
-
-            playerMovementScript.enabled = false;
-
-            await ShowTexts();
-
-            playerMovementScript.enabled = true;
-            animator.SetBool("isTalking", false);
-            playerAnimator.SetBool("isInDialog", false);
-            DialogUI.SetActive(false);
+            if (textComponent.text == lines[index])
+                NextLine();
+            
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index]; // Mostra direttamente tutta la frase
+            }
         }
     }
-    private async Task ShowTexts()
+
+    IEnumerator StartDialogue()
     {
-        for(int i=0; i<frasi.Length; i++)
+        isDialogueActive = true;
+        detectionRadius = -1; // Evita che il dialogo si riattivi
+
+        Debug.Log("Il player è vicino all'oggetto!");
+        animator.SetBool("isTalking", true);
+        playerAnimator.SetBool("isInDialog", true);
+        DialogUI.SetActive(true);
+        playerMovementScript.enabled = false;
+
+        index = 0;
+        yield return StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        textComponent.text = ""; // Pulisce il testo precedente
+        foreach (char c in lines[index].ToCharArray())
         {
-            await Task.Delay(1000);
-            Debug.Log(frasi[i]);
-            //Cambia testo dentro al panel
+            textComponent.text += c;
+            yield return new WaitForSeconds(TextSpeed);
         }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            StartCoroutine(TypeLine());
+        }
+        else
+            EndDialogue();
+        
+    }
+
+    void EndDialogue()
+    {
+        isDialogueActive = false;
+        playerMovementScript.enabled = true;
+        animator.SetBool("isTalking", false);
+        playerAnimator.SetBool("isInDialog", false);
+        DialogUI.SetActive(false);
     }
 }
