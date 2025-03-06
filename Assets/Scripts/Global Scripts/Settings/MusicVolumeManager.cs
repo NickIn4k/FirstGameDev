43720474a8bdf29e297e0d95f2d6d954f9354541
AndreaScuola <162697678+AndreaScuola@users.noreply.github.com>
2025-03-06 18:39:39 +0100

    v0.4pF - Small changes
    -Added a Realtime spot light to the player's helmet
    -Modified the Music Volume Manager script

--- Assets/Scripts/Global Scripts/Settings/MusicVolumeManager.cs
+++ Assets/Scripts/Global Scripts/Settings/MusicVolumeManager.cs
@@ -1,23 +1,26 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 //TEST DEL CODICE PER IL VOLUME DELLA MUSICA
-public class AudioSourceVolumeManager : MonoBehaviour
+public class MusicVolumeManager : MonoBehaviour
 {
     GenericSliderManager volumeManager;
     [SerializeField] private Slider volumeSlider;
-    [SerializeField] private AudioSource audioSource;
-    private const string VolumeKey = "AudioSourceVolume"; // Chiave per salvare il volume dell'AudioSource
+    [SerializeField] private AudioSource[] audioSources;
+    private const string VolumeKey = "AudioSourceVolume"; //Chiave per salvare il volume dell'AudioSource
 
     void Start()
     {
         volumeManager = new GenericSliderManager(volumeSlider, VolumeKey);
         volumeManager.SetStart((volume) =>
         {
-            // Aggiorna il volume del componente AudioSource
-            audioSource.volume = volume;
+            //Aggiorna il volume del componente AudioSource
+            foreach(AudioSource audio in audioSources)
+                audio.volume = volume;
             volumeManager.SavePrefab(volume);
         });
-        // Imposta il volume iniziale recuperato
-        audioSource.volume = volumeManager.GetPrefab();
+        //Imposta il volume iniziale recuperato
+        foreach (AudioSource audio in audioSources)
+            audio.volume = volumeManager.GetPrefab();
     }
 }
